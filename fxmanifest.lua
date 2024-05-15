67dbbe6f5441be673e93c23c3df68f0beca7a8c0
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-05-15 02:27:49 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,19 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 game 'rdr3'
 version '1.0'
 author 'VORP @YourgenAP'
-
-client_scripts {
-    'client/client.lua'
-}
-
-server_scripts { 
-    'server/server.lua'
-}
+description 'Saloon job for VORP Core'
 
 shared_scripts {
     'config.lua',
     'locale.lua',
     'languages/*.lua'
 }
---------------------------------------------------------------------------------------
+client_script 'client/client.lua'
+server_script 'server/server.lua'
 
----------------- Dependencies -------------------------------------------------------
----- What other scripts (if any) does your script depend on. REMOVE THIS IF NONE ----
+
 dependencies {
     'vorp_crafting',
 }
